92160c23b3ebbff120bc56db9b0e5dc5820a9c72
MySystemLagz <mousewalker2@gmail.com>
2019-06-17 19:48:30 -0500

    Got the name and location of the weather stations in and around Houston

--- WeatherRadar.py
+++ WeatherRadar.py
@@ -1,41 +1,18 @@
-# Given an list of at least 3 points, calculate whether x is within the bounds of those points
+# Given an list of at least 3, calculate whether x is within the bounds of those points
+# Use the shapely package
+# Use Fiona or shape to read .shp files, but first get it working
+# Use point.within(shape) or shape.contains(point) to check if a point is within a shape
 
-# Variables
-x = [0, 0, 5, 5]
-y = [0, 5, 5, 0]
+from shapely.geometry import Polygon, Point
+# import fiona
 
-class sq_extent():
-    right_x = 0
-    left_x = 0
-    up_y = 0
-    down_y = 0
+point = Point(2, 2)
+poly = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
 
-class point():
-    x = 2
-    y = 2
+# A list of stations, name and location, represented as points
+stations = [['KDWH', (30.062, -95.553)], ['KIAH', (29.985, -95.342)], ['KTME', (29.8050278, -95.8978889)], ['KHOU', (29.646, -95.279)], ['KSGR', (29.622, -95.657)], ['KLVJ', (29.521, -95.242)], ['KAXH', (29.5061389, -95.4769167)]]
 
-for num in x:
-    if num > sq_extent.right_x:
-        sq_extent.right_x = num
-    if num < sq_extent.left_x:
-        sq_extent.left_x = num
-    else:
-        continue
-
-for num in y:
-    if num > sq_extent.up_y:
-        sq_extent.up_y = num
-    if num < sq_extent.down_y:
-        sq_extent.down_y = num
-    else:
-        continue
-
-print('Upper extent:', sq_extent.up_y)
-print('Lower extent:', sq_extent.down_y)
-print('Right extent:', sq_extent.right_x)
-print('Left extent:', sq_extent.left_x)
-
-if point.x > sq_extent.left_x and point.x < sq_extent.right_x and point.y > sq_extent.down_y and point.y < sq_extent.up_y:
-    print('Within square bounds')
+if point.within(poly):
+    print('In bounds')
 else:
-    print('Not within square bounds')
+    print('Outta bounds')
