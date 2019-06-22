3c251b8e47cc1f7161e2a557054f736e84c96ef6
MySystemLagz <mousewalker2@gmail.com>
2019-06-21 21:33:45 -0500

    Used Fiona to read the shapefile

--- WeatherRadar.py
+++ WeatherRadar.py
@@ -1,15 +1,18 @@
 # Use Fiona, geopandas, or shape to read .shp files, but first get it working
 # Use point.within(shape) or shape.contains(point) to check if a point is within a shape
 
-from shapely.geometry import Polygon, Point, shape, MultiPolygon
-
-poly = Polygon([(0, 0), (5, 0), (5, 5), (0, 5)])
+from shapely.geometry import Polygon, Point, shape
+import fiona
 
 # A list of weather stations, name and location, represented as points
 stations = [['KDWH', (30.062, -95.553)], ['KIAH', (29.985, -95.342)], ['KTME', (29.8050278, -95.8978889)], ['KHOU', (29.646, -95.279)], ['KSGR', (29.622, -95.657)], ['KLVJ', (29.521, -95.242)], ['KAXH', (29.5061389, -95.4769167)]]
 
 # Extract information from shapefile here
-
+collection = fiona.open("wwa_201801010000_201812312359.shp")
+record = next(collection)
+coords = record['geometry']['coordinates'][0]
+# Convert to shapely geometry
+poly = Polygon(coords)
 
 # Loop through the weather stations and check if any of them are in bounds of "poly"
 # Update "poly" to the actual bounds of the flash flood weather reports
