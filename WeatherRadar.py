d4e3aaf08815c1c31c68d8021b372f037b961ace
MySystemLagz <mousewalker2@gmail.com>
2019-06-25 15:57:41 -0500

    Bug fixes

--- WeatherRadar.py
+++ WeatherRadar.py
@@ -5,37 +5,32 @@ from shapely.geometry import Polygon, Point, shape
 import fiona
 
 # A list of weather stations, name and location, represented as points
-stations = [['KDWH', (30.062, -95.553)], ['KIAH', (29.985, -95.342)], ['KTME', (29.8050278, -95.8978889)], ['KHOU', (29.646, -95.279)], ['KSGR', (29.622, -95.657)], ['KLVJ', (29.521, -95.242)], ['KAXH', (29.5061389, -95.4769167)]]
+stations = [['KDWH', (-95.553, 30.062)], ['KIAH', (-95.342, 29.985)], ['KTME', (-95.8978889, 29.8050278)], ['KHOU', (-95.279, 29.646)], ['KSGR', (-95.657, 29.622)], ['KLVJ', (-95.242, 29.521)], ['KAXH', (-95.4769167, 29.5061389)]]
 
-# Extract information from shapefile here
+# Open the shapefile
 collection = fiona.open("wwa_201801010000_201812312359.shp")
 
-# Loop through the weather stations and check if any of them are in bounds of the polygon
-counter = 0
-station_counter = 0
-in_bound = False
-while counter < 4107:
+# Loop through the weather reports and check if any weather station is in bounds of it
+for counter in range(4107):
+    in_bound = False
     record = next(collection)
     coords = record['geometry']['coordinates'][0]
+    # Check if it's even possible to convert the coordinates into a polygon
     if len(coords) >= 3:
         try:
             poly = Polygon(coords)
         except AssertionError:
-            counter += 1
             continue
     else:
-        counter += 1
         continue
 
-    while station_counter < len(stations):
+    for station_counter in range(len(stations)):
         location = Point(stations[station_counter][1])
         station = stations[station_counter][0]
         if location.within(poly):
+            if in_bound is False:
+                print('Index:', counter)
             print(station, '- In bounds')
             in_bound = True
-        station_counter += 1
-        if station_counter == len(stations) and in_bound == True:
-            print('-------------------')
-    counter += 1
-    station_counter = 0
-    in_bound = False
+        if station_counter == len(stations) - 1 and in_bound is True:
+            print('----------------')
