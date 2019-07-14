35c2478e325ec02973ac48b3fa5541162da4ccc6
Kyle Wodzicki <krwodzicki@gmail.com>
2019-07-14 22:11:19 +0100

    Refactors code for distribution
    Code is refactor so that it can be distrubuted/installed easily
    with pip.
    
    setup.py has been added as well as version file. No code has
    changes, just has been moved into the WeatherRadarML package
    within the WeatherRadarML git repo
    
    v0.0.1 KRW

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import setuptools
+from disutils.util import convert_path
+
+main_ns  = {};
+ver_path = convert)path("WeatherRadarML/version.py");       # Path to version file
+with open(ver_path) as ver_file:                            # Open version file for reading
+    exec( ver_file.read(), main_ns );                       # Parse data from version file into main_ns
+
+setuptools.setup(
+        name                = 'WeatherRadarML',
+        description         = 'Improving NEXRAD WSR-88D ZDR relation using machine learning.',
+        url                 = 'https://github.com/MySystemLagz/WeatherRadarML',
+        author              = 'Allen Dawodu',
+        version             = main_ns['__version__'],
+        packages            = setuptools.find_packages(),
+        install_requires    = ['fiona', 'shapely'],
+        package_data        = { '' : ['data/*.txt']},
+        zip_save            = False
+)
+
