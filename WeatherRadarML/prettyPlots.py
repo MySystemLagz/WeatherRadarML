1b8b2e4735fcf17c536fbee5fa4baf16582f87a6
MySystemLagz <mousewalker2@gmail.com>
2019-12-30 08:51:45 -0600

    Fixed radar radius

--- WeatherRadarML/prettyPlots.py
+++ WeatherRadarML/prettyPlots.py
@@ -4,12 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pyart.core import antenna_to_cartesian, cartesian_to_geographic
 
-def radar_range(station):
+def radar_range(station, kmrange=300.0):
     '''
     Purpose:
-        Plot a station and the range of it's radar
+        Plot a station and the range of its radar
     Inputs:
         station (String): The name of the station
+        kmrange (Float): The radius of the radar's range in km
     Keywords:
         None.
     Outputs:
@@ -30,11 +31,9 @@ def radar_range(station):
     lat = location[1]
 
     # Create a circle
-    xx, yy, zz = antenna_to_cartesian(.003, np.arange(360), 0)
-    # Offset the circle
-    for index in range(len(xx)):
-        xx[index] += lon
-        yy[index] += lat
+    xx, yy, zz = antenna_to_cartesian(kmrange, np.arange(360), 0)
+    proj = {'proj' : 'pyart_aeqd', 'lon_0' : lon, 'lat_0' : lat}
+    xx, yy = cartesian_to_geographic(xx, yy, proj)
 
     # Create the map
     ax = utils.baseMap()
@@ -44,4 +43,4 @@ def radar_range(station):
     
 
 if __name__ == "__main__":
-    test('HOU')
+    radar_range('HOU', 300)
